9a51706878a5ef302e807f23d898970f39402431
fanat1kq <110695423+fanat1kq@users.noreply.github.com>
2024-01-15 15:06:09 +0300

    +2

---
+++ ListNode.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class ListNode {
+//    public ListNode reverseList(ListNode head) {
+//        ListNode previosNode=null;
+//        ListNode currentNode=head;
+//        while(currentNode!=null){
+//            ListNode nextNode=currentNode.next;
+//            currentNode.next=previosNode;
+//            previosNode=currentNode;
+//            currentNode=nextNode;
+//
+//        }
+//        return previosNode;
+//    }
+}
+
